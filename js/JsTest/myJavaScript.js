f4c440d74d3d59e245c877c4977b86951864554a
Matheus Zanetti <mhznet2nd@gmail.com>
2014-05-08 20:42:36 -0300

    Teste de JS

---
+++ js/JsTest/myJavaScript.js
@@ -0,0 +1,12 @@
+function left()
+{
+    alert("Thanks good sir");
+    document.getElementById("demo").innerHTML = Date();
+}function right()
+{
+    alert("Yea right..");
+}
+function submit()
+{
+    alert("Thanks for submitting your answers.");
+}
