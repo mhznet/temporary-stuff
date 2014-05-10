3e999cc25cd60bdb3f0bb85087892a91a99dfed9
Matheus Zanetti <mhznet2nd@gmail.com>
2014-05-09 22:14:18 -0300

    Continua JS

---
+++ js/JsTest/QAFile.js
@@ -0,0 +1,25 @@
+function QAFile(){}
+QAFile.prototype.m_id = undefined;
+QAFile.prototype.m_index = undefined;
+QAFile.prototype.m_question = undefined;
+QAFile.prototype.m_answers = [];
+
+QAFile.prototype.setId = function (id)
+{
+    this.m_id = id;
+};
+QAFile.prototype.setIndex = function ()
+{
+    return this.m_index;
+};
+QAFile.prototype.setQuestion = function (question)
+{
+    this.m_question = question;
+};
+QAFile.prototype.setAnswers = function ()
+{
+    for (var i = 0; i < arguments.length; i++)
+    {
+        this.m_answers.push(arguments[i]);
+    }
+};
--- js/JsTest/myJavaScript.js
+++ js/JsTest/myJavaScript.js
@@ -10,3 +10,15 @@ function submit()
 {
     alert("Thanks for submitting your answers.");
 }
+
+var my_qa = new QAFile();
+my_qa.setId(0);
+my_qa.setQuestion("bla");
+my_qa.setAnswers("bla","bli","blu","ble");
+my_qa.setIndex(0);
+
+console.log(my_qa.m_id);
+console.log(my_qa.m_question);
+console.log(my_qa.m_answers);
+console.log(my_qa.m_index);
+
