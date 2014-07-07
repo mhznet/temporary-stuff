72b5e6fbb8410df618168b86e951b965862160c8
Matheus Zanetti <mhznet2nd@gmail.com>
2014-07-06 23:44:06 -0300

    Pong@Lot

---
+++ cs/unity3d/Pong-a-Lot/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public KeyCode moveUp = KeyCode.UpArrow;
+	public KeyCode moveDown = KeyCode.DownArrow;
+	public float speed = 10.0f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(moveUp))
+		{
+			var vel = rigidbody2D.velocity;
+			vel.y = speed;
+			rigidbody2D.velocity = vel;
+		}
+		else if (Input.GetKey (moveDown))
+		{
+			var vel = rigidbody2D.velocity;
+			vel.y = speed * -1;
+			rigidbody2D.velocity = vel;
+		}
+		else if (!Input.anyKey)
+		{
+			var vel = rigidbody2D.velocity;
+			vel.y = 0.0f;
+			rigidbody2D.velocity = vel;
+		}
+		var velX = rigidbody2D.velocity;
+		velX.x = 0;
+		rigidbody2D.velocity = velX;
+	}
+}
---
+++ cs/unity3d/Pong-a-Lot/Assets/scripts/WallController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public BoxCollider2D upperWall;
+	public BoxCollider2D leftWall;
+	public BoxCollider2D rightWall;
+	public BoxCollider2D bottomWall;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
