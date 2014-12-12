4333fa6b6bcdd42a26c3c42c294d784d87c83ba1
Matheus Zanetti <mhznet2nd@gmail.com>
2014-12-12 00:55:41 -0200

    +batidas

---
+++ cs/unity3d/Bate-Rebate/Assets/BallBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+    public float speed = 15f;
+    void Start()
+    {
+        rigidbody2D.velocity = Vector2.one.normalized * speed;
+        //TODO RANDOM SAIDA
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "paddleLeft")
+        {
+            float y = hitFactor(transform.position, col.transform.position, ((BoxCollider2D)col.collider).size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            rigidbody2D.velocity = dir * speed;
+        }
+        if (col.gameObject.name == "paddleRight") 
+        {
+            float y = hitFactor(transform.position,col.transform.position,((BoxCollider2D)col.collider).size.y);
+            Vector2 dir = new Vector2(-1, y).normalized;
+            rigidbody2D.velocity = dir * speed;
+        }
+        //if paredes GOAL!
+    }
+    float hitFactor(Vector2 ballPos, Vector2 paddlePos, float paddleHeight)
+    {
+        return (ballPos.y - paddlePos.y) / paddleHeight;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- cs/unity3d/Bate-Rebate/Assets/BateScripts/BatePaddleComp.cs
+++ cs/unity3d/Bate-Rebate/Assets/BateScripts/BatePaddleComp.cs
@@ -4,18 +4,7 @@ using System.Collections;
 public class BatePaddleComp : MonoBehaviour {
 
     public bool _ia;
-    public bool _noTouch;
-    public KeyCode _up;
-    public KeyCode _down;
     public float _speed;
-    public float _maxMove;
-
-    public Vector2 _position;
-    public Vector2 _size;
-    public float _northBorder;
-    public float _southBorder;
-    public float _eastBorder;
-    public float _westBorder;
 
 	// Use this for initialization
 	void Start () {
