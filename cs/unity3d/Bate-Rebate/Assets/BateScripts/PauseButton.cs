f33df89ce002582113900938f7a12bf8205f35b3
Matheus Zanetti <mhznet2nd@gmail.com>
2014-12-12 17:37:03 -0200

    update

---
+++ cs/unity3d/Bate-Rebate/Assets/BateScripts/PauseButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour 
+{
+    public void OnClickPause()
+    {
+        Debug.Log("OnClickPause");
+        Application.LoadLevel("sceneName");
+    }
+}
